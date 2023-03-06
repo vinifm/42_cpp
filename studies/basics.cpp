920600fe7dc04dfa230e683ab2c0504efcfd2978
viferrei <vifm@outlook.com>
2023-03-06 18:23:53 -0300

    iostreams

---
+++ studies/basics.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   basics.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/04 17:42:54 by viferrei          #+#    #+#             */
+/*   Updated: 2023/03/06 18:14:25 by viferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace	std;
+
+int main()
+{
+	int		num;
+	string	str;
+
+	cout << "Hello world !" << endl;
+	cout << "Enter an integer and a string, respectively:" << endl;
+	cin >> num >> str;
+	cout << "You entered " << num << " and " << str << endl;
+}
