aa820f00bec50cd7f3de834c9fda01f08e08c869
viferrei <vifm@outlook.com>
2023-03-22 18:07:58 -0300

    change compiler in makefiles

--- m00/ex00/megaphone.cpp
+++ m00/ex00/megaphone.cpp
@@ -6,13 +6,14 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 17:17:56 by viferrei          #+#    #+#             */
-/*   Updated: 2023/03/13 18:06:06 by viferrei         ###   ########.fr       */
+/*   Updated: 2023/03/22 17:50:26 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* Returns the input in uppercase */
 
 #include <iostream>
+#include <cctype>	// toupper()
 
 int main(int argc, char **argv)
 {
