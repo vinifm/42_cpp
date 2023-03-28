4786d00c3d24723b8106fe07a0ce430b747c875c
viferrei <vifm@outlook.com>
2023-03-28 19:43:02 -0300

    ex00: replace loop with std::string::iterator

--- m00/ex00/megaphone.cpp
+++ m00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 17:17:56 by viferrei          #+#    #+#             */
-/*   Updated: 2023/03/27 20:39:14 by viferrei         ###   ########.fr       */
+/*   Updated: 2023/03/28 19:42:33 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,9 @@ int main(int argc, char **argv)
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < argc; i++) {
-		for (size_t j = 0; argv[i][j] != '\0'; j++)
-			std::cout << (char) std::toupper(argv[i][j]);
+		std::string	str = argv[i];
+		for (std::string::iterator it = str.begin(); it != str.end(); it++)
+			std::cout << (char) std::toupper(*it);
 	}
 	std::cout << std::endl;
 }
