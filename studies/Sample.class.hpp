13841de70ae66dd98ce33cb25c7bbfb1059a1279
Vinicius Ferreira <vifm@outlook.com>
2023-05-19 01:00:35 -0300

    readme assignment operator

---
+++ studies/Sample.class.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Sample.class.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/18 17:37:59 by viferrei          #+#    #+#             */
+/*   Updated: 2023/05/18 17:59:46 by viferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class Sample {
+public:
+	Sample(void);				// constructor
+	Sample(const Sample &copy);	// copy constructor
+
+
+}
