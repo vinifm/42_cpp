d1ec3c3d288f637422b81d5b091820a73b95cf37
viferrei <vifm@outlook.com>
2023-05-26 17:37:23 -0300

    m01ex01 allocate array.

--- m01/ex01/main.cpp
+++ m01/ex01/main.cpp
@@ -6,12 +6,22 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 17:36:13 by viferrei          #+#    #+#             */
-/*   Updated: 2023/05/24 18:03:58 by viferrei         ###   ########.fr       */
+/*   Updated: 2023/05/26 17:36:28 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 int	main(void) {
+	int	size;
+	Zombie* horde;
+
+	size = 5;
+	horde = zombieHorde(size, "Johnny");
+	for (int i = 0; i < size; i++) {
+		std::cout << "Zombie number " << i << ": ";
+		horde[i].announce();
+	}
+	// delete[] horde;
 	return (0);
 }
--- m01/ex01/zombieHorde.cpp
+++ m01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 17:05:46 by viferrei          #+#    #+#             */
-/*   Updated: 2023/05/24 18:04:13 by viferrei         ###   ########.fr       */
+/*   Updated: 2023/05/26 17:31:58 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 	the name passed as parameter. Return a pointer to the first zombie */
 Zombie*	zombieHorde(int N, std::string name) {
 	Zombie* horde = new Zombie[N];
-	// for (int i = 0; i < sizeof(*horde)/sizeof(horde[0]); i++)
-	// 	horde[i].setName(name);
+	for (int i = 0; i < N; i++)
+		horde[i].setName(name);
+	return (horde);
 }
