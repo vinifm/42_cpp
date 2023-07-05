57add6f773849d5f3dbb3257803e465bfd70322f
viferrei <vifm@outlook.com>
2023-07-05 17:30:24 -0300

    m04 ex00 done

--- m04/ex01/include/Animal.hpp
+++ m04/ex01/include/Animal.hpp
@@ -1,29 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/03 19:44:47 by viferrei          #+#    #+#             */
-/*   Updated: 2023/07/05 16:45:32 by viferrei         ###   ########.fr       */
+/*   Created: 2023/07/03 19:44:33 by viferrei          #+#    #+#             */
+/*   Updated: 2023/07/04 19:02:26by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CAT_HPP
-# define CAT_HPP
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#define	YELLOW	"\033[1;33m"
+#define	CYAN	"\033[1;36m"
+#define	PINK	"\033[1;38;5;218m"
+#define	MAGENTA	"\033[1;35m"
+#define	BLUE	"\033[1;34m"
+#define	RESET	"\033[0m"
 
 #include <iostream>
-#include "../include/Animal.hpp"
 
-class Cat : public Animal {
+class Animal {
 public:
-	Cat();
-	Cat(const Cat& copy);
-	Cat& operator=(const Cat& rhs);
-	~Cat();
+	Animal();
+	Animal(const Animal& copy);
+	Animal& operator=(const Animal& rhs);
+	virtual ~Animal();
 
 	virtual void makeSound() const;
+
+	std::string getType() const;
+	void		setType(std::string new_type);
 
 protected:
 	std::string	type;
