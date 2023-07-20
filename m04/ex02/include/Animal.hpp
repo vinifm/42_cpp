d485707112a854942de2ebd89953fb71ecd0d754
viferrei <vifm@outlook.com>
2023-07-20 17:42:08 -0300

    m04ex02 Animal.hpp  align colors

--- m04/ex02/include/Animal.hpp
+++ m04/ex02/include/Animal.hpp
@@ -13,16 +13,17 @@
 #ifndef ANIMAL_HPP
 # define ANIMAL_HPP
 
-#define	RED		"\033[1;31m"
-#define	GREEN	"\033[1;32m"
-#define	YELLOW	"\033[1;33m"
-#define	CYAN	"\033[1;36m"
-#define	WHITE	"\033[1;37m"
-#define	PINK	"\033[1;38;5;218m"
-#define	PURPLE	"\033[1;38;5;105m"
-#define	MAGENTA	"\033[1;35m"
-#define	BLUE	"\033[1;34m"
-#define	RESET	"\033[0m"
+#define	RED			"\033[1;31m"
+#define	GREEN		"\033[1;32m"
+#define	YELLOW		"\033[1;33m"
+#define	CYAN		"\033[1;36m"
+#define	WHITE		"\033[1;37m"
+#define	PINK		"\033[1;38;5;218m"
+#define	PURPLE		"\033[1;38;5;105m"
+#define	LIME_GREEN	"\033[1;38;5;154m"
+#define	MAGENTA		"\033[1;35m"
+#define	BLUE		"\033[1;34m"
+#define	RESET		"\033[0m"
 
 #include <iostream>
 #include "Brain.hpp"
