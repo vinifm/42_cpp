92af977d5ed113cc557bf0f11bc73dc1a03d0b6e
viferrei <vifm@outlook.com>
2023-07-28 18:02:18 -0300

    m07ex02 done

--- m07/ex02/Array.hpp
+++ m07/ex02/Array.hpp
@@ -6,33 +6,88 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 12:07:05 by viferrei          #+#    #+#             */
-/*   Updated: 2023/07/28 12:27:13 by viferrei         ###   ########.fr       */
+/*   Updated: 2023/07/28 17:20:36y viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+#include <iostream>
+
 template <typename T>
 class	Array
 {
+private:
+	T*				_arr;
+	unsigned int	_size;
+
 public:
-	Array()
+	Array<T>() : _arr(NULL), _size(0) {};
+
+	Array<T>(unsigned int n) : _size(n)
 	{
-
+		if (_size)
+			_arr = new T[n];
+		for (size_t i = 0; i < _size; i++)
+			_arr[i] = T();
 	};
-	Array(unsigned int n);
-	Array(const Array& copy);
-	Array& operator=(const Array& rhs);
 
-	T&	operator[](size-t i);
-	size_t	size() const;
+	Array<T>(const Array& copy) : _arr(NULL), _size(copy.size())
+	{
+		if (_size)
+			_arr = new T[_size];
+	};
+
+	~Array<T>()
+	{
+		if (_arr)
+			delete[] _arr;
+	};
+
+	Array<T>& operator=(const Array& rhs)
+	{
+		if (this != &rhs) {
+			if (_arr)
+				delete[] _arr;
+			_size = rhs.size();
+			_arr = new T[_size];
+			for (size_t i = 0; i < _size; i++)
+				_arr[i] = rhs._arr[i];
+		}
+		return *this;
+	};
+
+	T&	operator[](size_t i)
+	{
+		if (i >= 0 && i < _size)
+			return _arr[i];
+		throw IndexOutOfBoundsException();
+	};
+
+	const T&	operator[](size_t i) const
+	{
+		if (i >= 0 && i < _size)
+			return _arr[i];
+		throw IndexOutOfBoundsException();
+	};
+
+	size_t	size() const { return _size; };
 
 private:
 	class	IndexOutOfBoundsException: public std::exception {
-		public: virtual const char* what() const throw();
-	}
-
+		public: virtual const char* what() const throw() {
+			return "Index out of bounds!!!";
+		}
+	};
 };
+
+template <typename T>
+void	displayArray(const Array<T>& arr)
+{
+	for (size_t i = 0; i < arr.size(); i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+}
 
 #endif
--- m07/ex02/main.cpp
+++ m07/ex02/main.cpp
@@ -6,8 +6,81 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 12:06:52 by viferrei          #+#    #+#             */
-/*   Updated: 2023/07/28 12:06:59 by viferrei         ###   ########.fr       */
+/*   Updated: 2023/07/28 17:30:09by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+#include <iostream>
+#include <stdlib.h>	// srand, rand
+
+#define	RESET	"\033[0m"
+#define	MAGENTA	"\033[1;35m"
+
+void	title(std::string msg, std::string color, size_t size);
+
+void	testIntArray()
+{
+	title("INT ARRAY", MAGENTA, 50);
+	size_t	size = 5;
+
+	Array<int>	empty;
+	Array<int>	arr(size);
+	Array<int>	copy(size);
+
+	std::cout << "empty array size: " << empty.size() << std::endl;
+	std::cout << "1st array: ";
+	for (size_t i = 0; i < size; i++) {
+		arr[i] = i;
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl << "2nd array: ";
+	displayArray(copy);
+	std::cout << "2nd array after copy assignment: ";
+	copy = arr;
+	displayArray(copy);
+	std::cout << "1st array changed: ";
+	arr[0] = 42;
+	displayArray(arr);
+	std::cout << "2nd array: ";
+	displayArray(copy);
+	std::cout << "Index out of bounds: ";
+	try { arr[10]; }
+	catch (std::exception& e) {std::cout << e.what() << std::endl; }
+	copy = empty;
+	displayArray(copy);
+}
+
+void	testStringArray()
+{
+	title("STRING ARRAY", MAGENTA, 50);
+	Array<std::string>	arr(3);
+	Array<std::string>	copy(3);
+
+	arr[0] = "Mary had";
+	arr[1] = "a little";
+	arr[2] = "lamb";
+
+	displayArray(copy);
+	copy = arr;
+	displayArray(arr);
+	displayArray(copy);
+}
+
+int	main(void)
+{
+	testIntArray();
+	testStringArray();
+}
+
+/* Print formatted title */
+void	title(std::string msg, std::string color, size_t size) {
+	if (size < msg.size())
+		size = msg.size();
+	size_t	chars_left = size - msg.size();
+
+	std::cout << std::endl << color << "/*--- " << msg << " ";
+	for (size_t i = 0; i < chars_left; i++)
+		std::cout << "-";
+	std::cout << "*/" << RESET << std::endl;
+}
