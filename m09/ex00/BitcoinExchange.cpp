90c673b86cd5230648d163f5cef7a249980f3a15
viferrei <vifm@outlook.com>
2023-08-03 20:59:42 -0300

    m09ex00 functions to validate input

--- m09/ex00/BitcoinExchange.cpp
+++ m09/ex00/BitcoinExchange.cpp
@@ -6,17 +6,24 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 19:10:19 by viferrei          #+#    #+#             */
-/*   Updated: 2023/08/03 19:07:16 by viferrei         ###   ########.fr       */
+/*   Updated: 2023/08/03 20:58:09 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
+/*--- STATIC VARIABLES -------------------------------------------------------*/
+
+std::map<std::string, float>	BitcoinExchange::_dataMap;
+
 /*--- CONSTRUCTORS AND DESTRUCTOR --------------------------------------------*/
 
 BitcoinExchange::BitcoinExchange() {}
-BitcoinExchange::BitcoinExchange(const BitcoinExchange& copy) {}
-BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& rhs) {}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& copy) { (void)copy; }
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& rhs) {
+	(void)rhs;
+	return *this;
+}
 BitcoinExchange::~BitcoinExchange() {}
 
 /*--- MEMBER FUNCTIONS -------------------------------------------------------*/
@@ -28,26 +35,79 @@ void	BitcoinExchange::execute(std::ifstream& inputFile)
 	_saveCsvIntoMap();
 	std::getline(inputFile, line);
 	while (std::getline(inputFile, line)) {
-
+		_validateLine(line);
 	}
+}
+
+std::pair<std::string, float>	BitcoinExchange::_validateLine(std::string line)
+{
+	size_t		delimPos = line.find(" | ");
+	std::pair<std::string, float>	pair;
+
+	if (delimPos == std::string::npos) {
+		_errorMsg("delimiter not found", line);
+		pair.first = "\0";
+		pair.second = -1;
+	}
+	else {
+		pair.first = _validateDate(line.substr(0, delimPos));
+		pair.second = _validateValue(line.substr(delimPos + 3));
+	}
+	return pair;
+}
+
+std::string	BitcoinExchange::_validateDate(std::string date)
+{
+	struct tm	timeStruct;
+	const char*	timePtr = strptime(date.c_str(), "%Y-%m-%d", &timeStruct);
+
+	if (timePtr == NULL || *timePtr != '\0') {
+		_errorMsg("bad input", date);
+		return "\0";
+	}
+	return date;
+}
+
+float BitcoinExchange::_validateValue(std::string valueStr)
+{
+	std::istringstream	iss(valueStr);
+	float				value;
+
+	if (!(iss >> value))
+		_errorMsg("bad input", valueStr);
+	else if (value < 0)
+		_errorMsg("not a positive number", valueStr);
+	else if (value > 1000)
+		_errorMsg("number too large", valueStr);
+	else
+		return value;
+	return -1;
 }
 
 void	BitcoinExchange::_saveCsvIntoMap()
 {
 	std::ifstream	file;
 	std::string		line;
-	std::string		date;
+	float			exchangeRate;
 
 	file.open("data.csv", std::ifstream::in);
 	if (!file.is_open())
 		throw FailedToOpenException();
 	std::getline(file, line);
 	while (std::getline(file, line)) {
-		std::istringstream	iss(line);
+		size_t				commaPosition = line.find(",");
+		std::string			date = line.substr(0, commaPosition);
+		std::istringstream	afterComma(line.substr(commaPosition + 1));
 
-		std::getline(iss, date, ',');
-		_dataMap[date] =
+		afterComma >> exchangeRate;
+		_dataMap[date] = exchangeRate;
 	}
+}
+
+void	BitcoinExchange::_errorMsg(std::string desc, std::string input)
+{
+	std::cerr << RED "Error: " RESET << desc << " => " << input
+		<< std::endl;
 }
 
 /*--- EXCEPTIONS -------------------------------------------------------------*/
--- m09/ex00/BitcoinExchange.hpp
+++ m09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 19:10:46 by viferrei          #+#    #+#             */
-/*   Updated: 2023/08/03 19:05:08 by viferrei         ###   ########.fr       */
+/*   Updated: 2023/08/03 20:41:06 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,11 @@
 #include <fstream>	// ifstream
 #include <sstream>	// istringstream
 #include <time.h>	// strptime
+#include <limits.h>
 #include <map>
+
+#define RED		"\033[1;31m"
+#define	RESET	"\033[0m"
 
 class BitcoinExchange {
 public:
@@ -32,6 +36,10 @@ private:
 	static std::map<std::string, float>	_dataMap;
 
 	static void	_saveCsvIntoMap();
+	static std::pair<std::string, float>	_validateLine(std::string line);
+	static std::string						_validateDate(std::string date);
+	static float							_validateValue(std::string valueStr);
+	static void								_errorMsg(std::string desc, std::string input);
 
 	class	FailedToOpenException: public std::exception {
 		public: const char* what() const throw();
