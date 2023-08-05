455585b76c613d61532421ba6bcb9da9e65d76a3
viferrei <vifm@outlook.com>
2023-08-05 20:15:29 -0300

    m09ex01 rm try catch in main

--- m09/ex01/srcs/main.cpp
+++ m09/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 14:56:21 by viferrei          #+#    #+#             */
-/*   Updated: 2023/08/05 19:31:45 by viferrei         ###   ########.fr       */
+/*   Updated: 2023/08/05 20:14:31 by viferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,6 @@ int main(int argc, char** argv)
 		std::cerr << "Missing argument" << std::endl;
 	else if (argc > 2)
 		std::cerr << "Too many arguments" << std::endl;
-	else {
-		try { RPN::execute(argv[1]); }
-		catch (std::exception& e) {std::cout << e.what() << std::endl; }
-	}
+	RPN::execute(argv[1]);
 	return 0;
 }
